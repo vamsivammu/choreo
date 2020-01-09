4cfae6fe1a32f46d306b32f90eb16d6d743e5109
2vrk1504 <vallabh.developer@gmail.com>
2020-01-09 10:57:20 +0530

    updated pg

--- pg_email_sender.js
+++ pg_email_sender.js
@@ -17,15 +17,36 @@ var admin_headers = {
 }
 
 var get_proshow_transactions = {
-    "type":"select",
-    "args":{
-        "table":"proshow_ticket_transactions",
-        "where":{
-         "$and":[
-             {"payment_id":{"$ne":null}}
-         ]   
+    "type": "select",
+    "args": {
+        "table": "proshow_ticket_transactions",
+        "columns": ['hasura_id', 'order_id', 'payment_id', 'ticket_data'],
+        "where": {
+            "$and": [
+                {
+                    "payment_id": {
+                        "$ne": null
+                    }
+                },
+                {
+                    "signature": {
+                        "$ne": null
+                    }
+                }
+            ]
         },
-        "columns":["hasura_id","ticket_data","order_id","payment_id"]
+        "order_by": [
+            {
+                "column": "hasura_id",
+                "order": "asc"
+            },
+            {
+                "column": "payment_id",
+                "order": "asc"
+            }
+        ],
+        "limit": 100,
+        "offset": 0
     }
 }
 var hasura_id_email={}
@@ -43,11 +64,13 @@ function get_num(obj){
     }
 }
 function start_sending_emails(index){
-    var email = actual_data[index].email
+
+    var hasura_id = actual_data[index].hasura_id
+    var email = hasura_id_email[hasura_id.toString()]
     var ticket_data = actual_data[index].ticket_data
     var payment_id = actual_data[index].payment_id
     var order_id = actual_data[index].order_id
-    var hasura_id = actual_data[index].hasura_id
+    
     var signature = crypto.createHmac('SHA256', KEY_SECRET).update(order_id + '|' + payment_id).digest('hex')
     var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -58,17 +81,17 @@ function start_sending_emails(index){
     });
     console.log(signature)
     if(!email.includes("@")){
-        console.log('email doesnt contain @'+ hasura_id.toString())
+        console.log('email doesnt contain @: '+ hasura_id.toString())
         start_sending_emails(index+1)
         return
     }
-    var qr_png = QRCode.image(signature.toString(), { type: 'png' });
+    var qr_png = QRCode.image({hasura_id:hasura_id, signature:signature.toString()}, { type: 'png' });
     qr_png.pipe(fs.createWriteStream(__dirname + `/views/temp/${order_id}.png`));
     
     let html_src = ejs.render(fs.readFileSync(__dirname + '/views/proshow_ticket.ejs', 'utf8'), {
         ticket_table_data: ticket_data,
         ticket_holder: email,
-        saarang_id: 'SA20U' + hasura_id,
+        saarang_id: 'SA20U' + String(hasura_id).padStart(5, '0'),
         order_id: order_id
     });
 
@@ -101,7 +124,7 @@ function start_sending_emails(index){
                         start_sending_emails(index+1)
                     }
                     fs.unlink(__dirname + `/views/temp/${order_id}.png`, function (err) {
-                        if (err) throw err;
+                        if (err) {throw err};
                         // if no error, file has been deleted successfully
                         console.log('File deleted!');
                     });
